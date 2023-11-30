0766aa33925304444202325e5e940ffdc10602aa
Shristi Gupta <shristiguptawork@gmail.com>
2023-11-30 21:35:13 +0530

    removed project slider

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,13 +1,20 @@
 'use client'
 import { PageContainer } from "@/components/layouts/page-container"
 import { ViewContainer } from "@/components/layouts/view-container"
-import { Slider } from "@/components/ui/Slider"
 
 const Projects = () => {
     return(
-        <PageContainer className="flex flex-col gap-18 bg-neutral-800 text-neutral-100 h-screen w-full p-6 ">
-            <h1 className="font-semibold text-5xl mx-auto">Projects</h1>
-            <Slider />
+        <PageContainer className="flex flex-col gap-18 bg-neutral-800 text-neutral-100 w-full p-6 overflow-x-hidden">
+            <marquee behavior="" direction=""><div className="text-neutral-700 cursor-default select-none flex gap-3 items-center">
+            <h1 className="font-semibold text-5xl ">Projects</h1>
+            <h1 className="font-semibold text-5xl ">Projects</h1>
+            <h1 className="font-semibold text-5xl ">Projects</h1>
+            <h1 className="font-semibold text-5xl ">Projects</h1>
+            <h1 className="font-semibold text-5xl ">Projects</h1>
+            <h1 className="font-semibold text-5xl ">Projects</h1>
+            <h1 className="font-semibold text-5xl ">Projects</h1>
+            <h1 className="font-semibold text-5xl ">Projects</h1>
+            </div></marquee>
         </PageContainer>
     )
 }

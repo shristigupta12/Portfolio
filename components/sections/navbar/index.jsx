44a53142acd79c6c1f99fc729dd315b42673ee30
shristigupta12 <shristiguptawork@gmail.com>
2024-07-02 01:38:15 +0530

    responsive: project section

--- components/sections/navbar/index.jsx
+++ components/sections/navbar/index.jsx
@@ -35,8 +35,8 @@ const Navbar = ()=>{
     ]
     return(
         <ViewContainer className={cn("view-container sticky top-3 flex justify-start")} id="nav">
-            <nav className="flex w-fit rounded-2xl backdrop-blur-sm bg-neutral-800   p-2 max-[500px]:px-2 max-[500px]:py-2 items-center transition-all shadow-md shadow-neutral-400 ">
-            <ul className="flex  gap-1 max-[500px]:gap-2  w-full text-neutral-700 max-[500px]:text-[14px] ">
+            <nav className="flex w-fit rounded-2xl backdrop-blur-sm bg-neutral-800 mt-3  p-2 max-[500px]:px-2 max-[500px]:py-2 items-center transition-all shadow-md shadow-neutral-400 ">
+            <ul className="flex  gap-1 max-[500px]:gap-2  w-full  text-neutral-700 max-[500px]:text-[14px] ">
                 {navItems.map((item, index)=>(
                     <Link href={item.link} key={index}>
                     <li className="cursor-pointer text-neutral-50 hover:bg-neutral-50  hover:text-neutral-900  px-3 py-1.5 rounded-xl  max-[500px]:px-1 max-[500px]:py-1">{item.name}</li>
--- components/sections/projects/index.jsx
+++ components/sections/projects/index.jsx
@@ -12,23 +12,28 @@ const Projects = () => {
                 <h1 className="font-semibold text-5xl max-md:text-4xl">Projects</h1>
                 <div className="grid gap-20 w-full my-7">
                     {ProjectsData.map((project, index)=>(
-                        <div key={index} className="flex justify-center items-center gap-12 max-md:flex-col-reverse">
+                        <div key={index} className="flex justify-center items-center gap-12">
                             {/* <Link href={project.link} className="block"> */}
-                            <Image src={project.img} alt={"project image"} width={'400'} height={'280'} className="w-[500px] h-[280px] rounded-xl cursor-pointer hover:scale-95 transition-all max-lg:w-[300px] max-lg:h-[210px] max-md:w-[500px] max-sm:h-[280px]" onClick={()=>{
+                            <Image src={project.img} alt={"project image"} width={'400'} height={'280'} className="w-[500px] h-[280px] rounded-xl cursor-pointer hover:scale-95 transition-all max-lg:w-[300px] max-lg:h-[210px] max-md:w-[500px] max-sm:h-[280px] max-md:hidden" onClick={()=>{
                                 window.open(project.link)
                             }}/>
                             {/* </Link> */}
-                            <div className="flex flex-col justify-between gap-6">
+                            <div className="flex flex-col justify-between gap-6 ">
                                 <h1 className="text-4xl max-md:text-2xl  ">{project.name}</h1>
                                 <p className="max-[500px]:text-[14px]">{project.desc}</p>
-                                <div key={index} className="flex gap-3 justify-center">
+                                <div key={index} className="flex gap-3 justify-center ">
                                     {project.techStack.map((tech, index)=>(
-                                        <Badge variant="outline" key={index} className=" cursor-pointer hover:bg-neutral-200 hover:text-neutral-900 hover:scale-105">{tech}</Badge>
+                                        <Badge variant="default" key={index} className=" cursor-pointer font-extrabold hover:bg-neutral-200 hover:text-neutral-900 hover:scale-105">{tech}</Badge>
                                     ))}
                                 </div>
-                                <Link href={project.githubLink} target="_blank">
-                                    <Button variant="default" className="rounded-md w-full hover:scale-105">{"Github -->"}</Button>
-                                </Link>
+                                <div className="flex justify-between min-md:hidden">
+                                    <Link href={project.githubLink} target="_blank">
+                                        <Button variant="default" className="rounded-xl  w-full hover:scale-105 shadow-md shadow-neutral-400">{"Github -->"}</Button>
+                                    </Link>
+                                    <Link href={project.link} target="_blank">
+                                        <Button variant="default" className="rounded-xl  w-full hover:scale-105 shadow-md shadow-neutral-400">{"Deployment -->"}</Button>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     ))}
